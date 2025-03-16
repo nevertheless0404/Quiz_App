b406c8ef37097996ee3db9e9a2def6a5d83b8605
nevertheless0404 <nevertheless.hi@gmail.com>
2025-03-16 22:23:45 +0900

    수정

--- fastapi/database.py
+++ fastapi/database.py
@@ -3,7 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost/quiz_app"
+from dotenv import load_dotenv
+import os
+
+# .env 파일 로드
+load_dotenv()
+
+# 환경 변수에서 데이터베이스 URL 가져오기
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 
 # 비동기 엔진 생성
